3e529a81597fc96d5ba46e74e8692399448e84f8
Charith Sathsara <charithsathsara169@gmail.com>
2022-07-18 23:06:00 +0530

    Committing Tutorial - 03 Question 05

--- src/main/scala/functionalprogramming/tutorial03/Question05.scala
+++ src/main/scala/functionalprogramming/tutorial03/Question05.scala
@@ -2,10 +2,10 @@ package functionalprogramming.tutorial03
 
 object Question05 extends App {
 
-  //Calculate running time for easy phase
+  //Calculate running time for easy phase...
   def easy(km:Int):Int = km*8;
 
-  //Calculate running time for tempo phase
+  //Calculate running time for tempo phase...
   def tempo(km:Int):Int = km*7;
 
   var total = easy(2) + tempo(3) + easy(2);
