8b5220238fb204a9031067c78244f203d03c0caf
Charith Sathsara <charithsathsara169@gmail.com>
2022-07-18 13:58:19 +0530

    Committing Tutorial - 03 Question 01

---
+++ src/main/scala/functionalprogramming/tutorial02/Question01.scala
@@ -0,0 +1,12 @@
+package functionalprogramming.tutorial02
+
+import scala.math.Pi
+
+object Question01 extends App {
+
+  //Calculate area of a disk using given radius
+  def areaOfDisk(radius:Double):Double=radius*radius*Pi;
+
+  printf("Area of disk with radius 5 is = %.2f", areaOfDisk(5));
+
+}
