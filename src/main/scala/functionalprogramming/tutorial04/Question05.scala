1cebc38ea1c0551f1c229790492a248c887d4270
Charith Sathsara <charithsathsara169@gmail.com>
2022-08-02 01:59:55 +0530

    Updating Tutorial - 04 Question 05

--- src/main/scala/functionalprogramming/tutorial04/Question05.scala
+++ src/main/scala/functionalprogramming/tutorial04/Question05.scala
@@ -9,12 +9,37 @@ object Question05 extends App {
   val toLower = (input:String)=> input.toLowerCase();
 
   //High Order Function that is used to convert the name into given format
-  val formatNames = (format:(String)=>String,name:String) => format(name);
+  //val formatNames = (format:(String)=>String,name:String) => format(name);
+
+  //High Order Function that is used to convert the name into given format
+  //This is also a recursive function
+  def formatNames(name:String,format:(String)=>String,indexSequence:Seq[Int]):String = {
+
+    if(indexSequence.isEmpty){
+      return name;
+    }
+
+    if(indexSequence.head < name.length()){
+
+      var tempName = "";
+
+      indexSequence.head match{
+        case 0 => tempName = format(name.charAt(indexSequence.head).toString) + name.substring(indexSequence.head + 1);
+        case _ => tempName = name.substring(0,indexSequence.head) + format(name.charAt(indexSequence.head).toString) + name.substring(indexSequence.head + 1);
+      }
+
+      formatNames(tempName,format, indexSequence.tail);
+
+    }else{
+       "Please Enter Valid Indexes of the name";
+    }
+
+  }
 
   printf("\nOutput :\n");
-  printf(formatNames(toUpper,"Benny") + "\n");
-  printf(formatNames(toUpper,"Niroshan") + "\n");
-  printf(formatNames(toLower,"Saman") + "\n");
-  printf(formatNames(toUpper,"Kumara") + "\n");
+  printf(formatNames("Benny",toUpper,Seq(0,1,2,3,4)) + "\n");
+  printf(formatNames("Niroshan",toUpper,Seq(0,1)) + "\n");
+  printf(formatNames("Saman",toLower,Seq(0)) + "\n");
+  printf(formatNames("Kumara",toUpper,Seq(0,5)) + "\n");
 
 }
