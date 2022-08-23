cb29e11670ab3b3efaaa4da7868bab1dc65f1552
Charith Sathsara <charithsathsara169@gmail.com>
2022-08-24 01:26:29 +0530

    Committing Tutorial - 05 Question 01

--- src/main/scala/functionalprogramming/tutorial05/Question01.scala
+++ src/main/scala/functionalprogramming/tutorial05/Question01.scala
@@ -2,11 +2,18 @@ package functionalprogramming.tutorial05
 
 object Question01 extends App {
 
+  //Greatest common divisor recursion function
+  def GCD(a:Int,b:Int):Int = b match{
+    case 0 => a
+    case x if x>a => GCD(x,a)
+    case _ => GCD(b,a%b)
+  }
+
   //Tail recursion function
   def prime(n:Int,d:Int=2):Boolean = d match {
 
     case d if(n==d) => true;
-    case d if(n%d == 0) => false;
+    case d if GCD(n,d) > 1 => false;
     case d => prime(n,d+1);
 
   }
