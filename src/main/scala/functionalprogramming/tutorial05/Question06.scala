8c2fdb56100c61a5c5c2469a2b2176c03809364a
Charith Sathsara <charithsathsara169@gmail.com>
2022-08-24 03:31:57 +0530

    Committing Tutorial - 05 Question 06

---
+++ src/main/scala/functionalprogramming/tutorial05/Question06.scala
@@ -0,0 +1,19 @@
+package functionalprogramming.tutorial05
+
+object Question06 extends App {
+
+  //Binary recursive function
+  def fibonacci(n:Int):Int= n match{
+    case 0 => 0;
+    case x if x==1 => 1;
+    case _ => fibonacci(n-1)+fibonacci(n-2);
+  }
+
+  def fibonacciSeq(n:Int):Unit= {
+    if (n > 0) fibonacciSeq(n-1);
+    println(fibonacci(n));
+  }
+
+  fibonacciSeq(5);
+
+}
