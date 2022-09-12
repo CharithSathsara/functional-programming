f2c1d5f18b0a86dbc0ad0f36e37df99e57c9333f
Charith Sathsara <charithsathsara169@gmail.com>
2022-09-13 03:33:18 +0530

    Committing Tutorial - 07 Question 01 - Update

--- src/main/scala/functionalprogramming/tutorial07/Question01.scala
+++ src/main/scala/functionalprogramming/tutorial07/Question01.scala
@@ -2,11 +2,23 @@ package functionalprogramming.tutorial07
 
 object Question01 extends App {
 
-  val x = new Rational(8,3);
-  printf(" X =  %s\n",x.toString);
+  val p = new Rational(8,3);
+  printf(" P =  %s\n",p.toString);
 
-  val negX = x.neg;
-  printf("-X = %s\n",negX.toString);
+  val negP = p.neg;
+  printf("-P = %s\n\n",negP.toString);
+
+  val x = new Rational(8, 3);
+  val y = new Rational(5, 7);
+  val z = new Rational(7, 3);
+
+  val result = x-y-z;
+
+  printf("X = %s\n",x.toString);
+  printf("Y = %s\n",y.toString);
+  printf("Z = %s\n",z.toString);
+
+  printf("\nResult of X-Y-Z = %s", result.toString);
 
 }
 
@@ -18,8 +30,15 @@ class Rational(n:Int, d:Int){
   //Greatest Common Divisor Function
   private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b);
 
+  //plus method
+  def +(r:Rational) = new Rational(this.numerator*r.denominator + this.denominator*r.numerator,
+                                                                this.denominator*r.denominator)
+
   //neg method that is used like x.neg => evaluates to -x
   def neg = new Rational(-this.numerator,this.denominator);
+
+  //minus method => x - y = x + (-y)
+  def -(r:Rational) = this + r.neg;
 
   //overriding the toString method
   override def toString: String = numerator + "/" + denominator;
